7f02872d2156ed49d2b2eac7bbdbf3771c846f8e
github username <github email address>
2018-07-02 15:22:05 -0400

    add Workout view pages

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,2 +1,25 @@
 class ExercisesController < ApplicationController
+
+  def new
+    @exercise = Exercise.new
+  end
+
+  def create
+    @exercise = Exercise.new(exercise_params)
+  end
+
+
+
+private
+
+  def exercise_params
+    params.require(:exercise).permit(
+    :name,
+    :description,
+    :sets,
+    :reps,
+    :rest
+    )
+  end
+
 end
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,59 @@
 class WorkoutsController < ApplicationController
+  before_action :set_workout, only: [:show, :edit, :update, :destroy]
+
+
+    def index
+      @workouts = Workout.all
+    end
+
+    def new
+      @workout = Workout.new
+    end
+
+    def create
+      @workout = Workout.new(workout_params)
+        if @workout.save
+          #session[:workout_id] = @workout.id
+          redirect_to workout_path(@workout)
+        else
+          render :new
+        end
+    end
+
+    def edit
+
+    end
+
+    def update
+      if @workout.update(workout_params)
+         redirect_to @workout
+       else
+         render :edit
+       end
+    end
+
+    def destroy
+      session[:workout_id] = nil
+      redirect_to workouts_path
+    end
+
+
+
+
+
+
+    private
+      def workout_params
+        params.require(:workout).permit(
+        :name,
+        :body_part,
+        :description
+        )
+      end
+
+      def set_workout
+        @workout = Workout.find(params[:id])
+      end
+
+
 end
